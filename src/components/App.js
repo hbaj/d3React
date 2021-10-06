8528ec2aecaa53ca8a3ae75f8a65410b79c602a5
hbaj <hbaj_dimmu@hotmail.com>
2021-10-05 20:02:39 -0500

    D3 axis added - initial stage

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,29 @@ import snoopy from "../images/snoopy.jpg";
 import AllPeople from "./quest";
 import axios from "axios";
 import React, { useState, useRef, useEffect } from "react";
-import { select, scaleLinear, axisLeft } from "d3";
+import { select, scaleLinear, scaleTime, timeParse,axisLeft, axisBottom, min, max, extent } from "d3";
 
 function App() {
   console.log(`1-initial data->:`, r);
 
-  const [r, setR] = useState([5]);
+  const [r, setR] = useState(null);
   const [flag, setFlag] = useState(0);
   const svgRef = useRef();
-  var x = "hola";
+  // var x = "hola";
   const djangoApiUrl = "http://192.168.1.222:8000/var-meassures/";
 
-  const screen_props = {
-    width: document.body.clientWidth - 50,
-    height: document.body.clientHeight - 200,
-    margin: { top: 30, bottom: 100, left: 100, right: 30 },
-    tickSeparationRatio: 70,
-  };
+  function props() {
+    let width = document.body.clientWidth;
+    let height = document.body.clientHeight;
+    let margin = { top: 30, bottom: 100, left: 100, right: 30 };
+    let tickSeparationRatio = 70;
+    return {
+      width: width,
+      height: height,
+      margin: margin,
+      tickSeparationRatio: tickSeparationRatio,
+    };
+  }
 
   console.log("2-initial data->:", r);
 
@@ -27,12 +33,15 @@ function App() {
     try {
       const datos = await axios.get(djangoApiUrl);
 
-      console.log("datos after promise completion:", datos);
+      // console.log("datos after promise completion:", datos);
       const datosArray = datos.data.results;
       // const datosArray = datos_.map((d) => +d.value * Math.random());
       console.log("array fixed from axios:", datosArray);
       setR(datosArray);
-      console.log("updating state:", datosArray);
+      console.log(
+        "***************Axios request complete - Updating state :",
+        datosArray
+      );
 
       // console.log(resp.data);
     } catch (err) {
@@ -41,69 +50,143 @@ function App() {
     }
   };
 
+  function calculateAxisProperties(axisData) {
+    let b = min(axisData);
+    let c = max(axisData);
+    let d = extent(axisData);
+    return [b, c, d];
+  }
+
   useEffect(() => {
     console.log("1--inside useEffect function", r);
-    // setR(100);
-
+    const { width, height, margin, tickSeparationRatio } = props();
     sendGetRequest();
-    //   setTimeout(() => {
-    //     console.log('inside timeout function')
-    //     setR(Math.random()*50)
-    //   }, 3000);
-    x = "noHola";
-    console.log("2--inside useEffect function", r);
-    console.log(" xvalue inside useEffect function->:", x);
+    // x = "noHola";
+    console.log("AXIOS DATA--inside useEffect function", r);
+    // console.log(" xvalue inside useEffect function->:", x);
+    if (!r) {
+      console.log("inside !r check");
+      return null;
+    }
+    const xAxisProps = calculateAxisProperties(r.map((d) => d.date));
+    const parseDate = timeParse('%Y-%m-%d')
+    const yAxisProps = calculateAxisProperties(r.map((d) => d.value));
+    console.log(xAxisProps, "\n", yAxisProps);
+    const svg = select(svgRef.current)
+      .attr("width", width*0.7)
+      .attr("height", height*0.5)
+      .style("background-color", "green")
+      .selectAll("g")
+      .data([0])
+      .join("g")
+      .attr("class", "g-margin-plot")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .style("background-color", "purple");
+
+    console.log("x properties: ", xAxisProps);
+
+    // ########### x axis setup ############
+    const xScale = scaleTime()
+   /* enter data to modify y scale   */
+      .range([0, 200])
+      // .range([+yAxisProps.max,+yAxisProps.min])
+      .domain([parseDate('2021-09-26') , parseDate('2021-09-31')]);
+      // .domain([0, +yAxisProps.max]);
+
+      console.log('yScale:',xScale)
+    const xAxis = axisBottom(xScale)
+    // .ticks(
+    //   (height - margin.top) / tickSeparationRatio
+    // );
+    let xAxisG = svg.selectAll(".g-margin-plot")
+                    .data([0])
+                    .join('g')
+                    .attr("transform", "translate(0 " + '20' + ")")
+                    .call(xAxis)
+                    ;
+
+    // ########### y axis setup ############
+    const yScale = scaleLinear()
+   /* enter data to modify y scale   */
+      .range([0, 90])
+      // .range([+yAxisProps.max,+yAxisProps.min])
+      .domain([0, 800]);
+      // .domain([0, +yAxisProps.max]);
+
+      console.log('yScale:',yScale)
+    const yAxis = axisLeft(yScale)
+    // .ticks(
+    //   (height - margin.top) / tickSeparationRatio
+    // );
+    let yAxisG = svg.selectAll(".g-margin-plot")
+                    .data([0])
+                    .join('g')
+                    .attr("transform", "translate(0 " + '20' + ")")
+                    .call(yAxis)
+                    ;
+
+
+    svg
+      .selectAll("circle")
+      .data(r)
+      .join(
+        (enter) => enter.append("circle").attr("class", "new"),
+        (update) => update.attr("class", "udpated"),
+        (exit) => exit.remove()
+      )
+      .attr("r", 10)
+      .attr("cx", (r) => +r.value * Math.random())
+      .attr("cy", (r) => r.value)
+      .attr("stroke", "red");
+
     return () => {
       console.log("inside useEffect cleaning function");
     };
   }, [flag]);
 
-  console.log("3-initial data->:", r);
-  console.log(" xvalue->:", x);
-  console.log("flag =", flag);
-  console.log("incoming  svgREf processing ", svgRef.current);
-  // const xValueCount = r.length;
-  // console.log("rango  x", [...Array(xValueCount).keys()]);
+  // console.log("3-initial data->:", r);
+  // console.log(" xvalue->:", x);
+  // console.log("flag =", flag);
+  // console.log("incoming  svgREf processing ", svgRef.current);
+  // // const xValueCount = r.length;
+  // // console.log("rango  x", [...Array(xValueCount).keys()]);
 
-  // // ########### y axis setup ############
-  // const yScale = scaleLinear()
-  //   .domain([0, 1]) /* enter data to modify y scale   */
-  //   .range([screen_props.innerHeight, 0]);
-  // const yAxis = axisLeft(yScale).ticks(screen_props.innerHeight / screen_props.tickSeparationRatio);
-  // let yAxisG = g.selectAll(".y-axis").data([null]);
-  // yAxisG = yAxisG
-  //   .join("g")
-  //   .attr("class", "y-axis") /* enter data to modify y scale Name   */
-  //   .merge(yAxisG);
-  // yAxisG.call(yAxis);
+  // // // ########### y axis setup ############
+  // // const yScale = scaleLinear()
+  // //   .domain([0, 1]) /* enter data to modify y scale   */
+  // //   .range([screen_props.innerHeight, 0]);
+  // // const yAxis = axisLeft(yScale).ticks(screen_props.innerHeight / screen_props.tickSeparationRatio);
+  // // let yAxisG = g.selectAll(".y-axis").data([null]);
+  // // yAxisG = yAxisG
+  // //   .join("g")
+  // //   .attr("class", "y-axis") /* enter data to modify y scale Name   */
+  // //   .merge(yAxisG);
+  // // yAxisG.call(yAxis);
 
-  const svg = select(svgRef.current)
-    .attr("width", 500)
-    .attr("height", 400)
-    .style("background-color", "green")
-    .selectAll('g')
-    .data([0])
-    .join('g')
-  .attr("transform", "translate(200 130)")
-  .style('background-color','purple')
+  // const svg = select(svgRef.current)
+  //   .attr("width", 500)
+  //   .attr("height", 400)
+  //   .style("background-color", "green")
+  //   .selectAll('g')
+  //   .data([0])
+  //   .join('g')
+  // .attr("transform", "translate(50 20)")
+  // .style('background-color','purple')
 
-  console.log("incoming datos to svg processing ", r);
-  console.log(
-    "value col passing to d3 as datos",
-    r.map((d) => d.value)
-  );
-  svg
-    .selectAll("circle")
-    .data(r)
-    .join(
-      (enter) => enter.append("circle").attr("class", "new"),
-      (update) => update.attr("class", "udpated"),
-      (exit) => exit.remove()
-    )
-    .attr("r", (value) => 20)
-    .attr("cx", (r) => +r.value * Math.random())
-    .attr("cy", (value) => 200)
-    .attr("stroke", "red");
+  // console.log("incoming datos to svg processing ", r);
+
+  // svg
+  //   .selectAll("circle")
+  //   .data(r)
+  //   .join(
+  //     (enter) => enter.append("circle").attr("class", "new"),
+  //     (update) => update.attr("class", "udpated"),
+  //     (exit) => exit.remove()
+  //   )
+  //   .attr("r", r => r.value)
+  //   .attr("cx", r => +r.value * Math.random())
+  //   .attr("cy", r => r.value)
+  //   .attr("stroke", "red");
 
   return (
     <React.Fragment>
