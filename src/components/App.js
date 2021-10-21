c88bf56532babc6f677bf9e422e13f25c6e3c0cb
hbaj <hbaj_dimmu@hotmail.com>
2021-10-21 05:04:20 -0500

    Navbar added to project with material UI.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
 import React, { useState, useRef, useEffect } from "react";
 import snoopy from "../images/snoopy.jpg";
 import ScatterPlot from "./Plots/Plots";
+import PrimarySearchAppBar from "./Bars/Navbar"
+import ClippedDrawer from "./Bars/SideBar"
 import {ContactForm} from "./Forms/form"
+import Box from '@mui/material/Box'
 function App() {
 
 
   return (
     <>
-      <ContactForm/>
-      <ScatterPlot/>
-      <ScatterPlot/>
-      <ScatterPlot/>
-      <ScatterPlot/>
+    <PrimarySearchAppBar/>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          p: 5,
+          m: 5,
+          overflowY:'auto',
+          maxWidth: 600,
+        }}
+      >
+        <Box sx={{ p: 5, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+        <Box sx={{ p: 5, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+        <Box sx={{ p: 1, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+        <Box sx={{ p: 1, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+        <Box sx={{ p: 1, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+        <Box sx={{ p: 1, bgcolor: 'grey.300' }}><ScatterPlot/></Box>
+      </Box>
+     
       <h1> hola from inside APP</h1>
      
     </>
